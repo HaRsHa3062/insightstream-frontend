681a56770b40f883367ca47df710f888894e3f9e
HaRsHa3062 <iragavarapu.harsha@yahoo.com>
2026-05-21 18:19:09 +0530

    Add layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'InsightStream AI',
+  description: 'Churn Analytics for Subscription Brands',
+};
+
+export default function RootLayout ({
+  children,
+}; {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+    <body>{children}</body>
+    </html>
+  );
+}
